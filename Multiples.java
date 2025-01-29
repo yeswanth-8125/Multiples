a60acd4ae6a99bddc23542b29765b0cdc6945f22
yeswanth-8125 <yeswanthkumarelluru@gmail.com>
2025-01-29 10:26:28 +0530

    Add files via upload

---
+++ Multiples.java
@@ -0,0 +1,8 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int a=10;
+        for(int i=1;i<=10;i++){
+            System.out.println(a+"*"+i+"="+a*i);
+        }
+    }
+}
